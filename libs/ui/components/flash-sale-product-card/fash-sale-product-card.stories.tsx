cb2ae086d2642d3e00a3ff04b02b2685c0734882
prj_1 <dongbui942006@gmail.com>
2026-09-16 09:36:37 +0700

    feat: flash sale product card

---
+++ libs/ui/components/flash-sale-product-card/fash-sale-product-card.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { FlashSaleProductCard } from '.';
+
+const meta = {
+  title: 'components/flashsalecard',
+  component: FlashSaleProductCard,
+  argTypes: {
+    stock: {
+      control: 'number',
+    },
+    sold: {
+      control: 'number',
+    },
+    sale: {
+      control: 'number',
+    },
+    originalPrice: {
+      control: 'number',
+    },
+    salePrice: {
+      control: 'number',
+    },
+  },
+} satisfies Meta<typeof FlashSaleProductCard>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {
+  render: (args) => <FlashSaleProductCard {...args} />,
+  args: {
+    sale: 20,
+    name: 'Bàn phím cơ AULA SF2099 RGB Mechanical Keyboard',
+    description: 'Bàn phím cơ ngon trong tầm giá',
+    originalPrice: 2000000,
+    salePrice: 1000000,
+    imageUrl:
+      'https://philong.com.vn/media/lib/04-03-2024/ban-phim-co-khong-day-edra-ek398l-philong2.jpg',
+    stock: 30,
+    sold: 120,
+  },
+};
+export const NoSale: Story = {
+  render: (args) => <FlashSaleProductCard {...args} />,
+  args: {
+    sale: 0,
+    name: 'Bàn phím cơ AULA SF2099 RGB Mechanical Keyboard',
+    description: 'Bàn phím cơ ngon trong tầm giá',
+    originalPrice: 2000000,
+    salePrice: 1000000,
+    imageUrl:
+      'https://philong.com.vn/media/lib/04-03-2024/ban-phim-co-khong-day-edra-ek398l-philong2.jpg',
+    stock: 30,
+    sold: 120,
+  },
+};
+
+export const SoldOut: Story = {
+  render: (args) => <FlashSaleProductCard {...args} />,
+  args: {
+    sale: 20,
+    name: 'Bàn phím cơ AULA SF2099 RGB Mechanical Keyboarddsadsadasdasdasdasdas',
+    description: 'Bàn phím cơ ngon trong tầm giá ,3 cổng kết nối 74 phím , led rgb, hot swap',
+    originalPrice: 2000000,
+    salePrice: 1000000,
+    imageUrl:
+      'https://philong.com.vn/media/lib/04-03-2024/ban-phim-co-khong-day-edra-ek398l-philong2.jpg',
+    stock: 0,
+    sold: 120,
+  },
+};
---
+++ libs/ui/components/flash-sale-product-card/index.tsx
@@ -0,0 +1,77 @@
+import { cn, formatCurrency } from '../../helpers';
+export type FlashSaleProductCardProps = {
+  className?: string;
+
+  sale: number;
+
+  imageUrl: string;
+
+  name: string;
+  description?: string;
+
+  originalPrice: number;
+  salePrice: number;
+
+  sold: number;
+  stock: number;
+
+  customClasses?: {
+    root?: string;
+    image?: string;
+    name?: string;
+    price?: string;
+  };
+};
+
+export function FlashSaleProductCard({
+  className,
+  sale,
+  imageUrl,
+  name,
+  description,
+  originalPrice,
+  salePrice,
+  sold,
+  stock,
+}: FlashSaleProductCardProps) {
+  const totalQuantity = stock + sold;
+  const soldPercent = totalQuantity > 0 ? (sold / totalQuantity) * 100 : 0;
+  const displayPrice = formatCurrency(salePrice);
+  const displayOriginalPrice = originalPrice ? formatCurrency(originalPrice) : null;
+  return (
+    <div className={` rounded-xl w-80 max-h-full border border-neutral-divider p-4  ${className}`}>
+      <div className="relative  rounded-xl w-full h-48">
+        {sale > 0 && (
+          <span className="absolute left-2 top-2 bg-error px-4 py-1 rounded-full text-neutral-white font-bold text-base">
+            -{sale}%
+          </span>
+        )}
+        {imageUrl && (
+          <img src={imageUrl} alt={name} className="w-full h-full object-cover rounded-xl" />
+        )}
+      </div>
+      <div className="mt-2">
+        <span className="text-neutral-black font-medium text-md line-clamp-2">{name}</span>
+        <span className="text-neutral-text-secondary text-xs line-clamp-1">{description}</span>
+        <div className="flex mt-2 ">
+          <div className="flex items-center gap-4 ">
+            <span className="text-primary text-xl font-bold">{displayPrice}</span>
+            {originalPrice && (
+              <span className="line-through text-neutral-text-secondary text-base">
+                {displayOriginalPrice}
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="w-full mt-2">
+        <div className="w-full bg-neutral-border rounded-full h-2">
+          <div className="bg-primary-base h-2 rounded-full" style={{ width: `${soldPercent}%` }} />
+        </div>
+        <p className="mt-2 text-sm text-[var(--color-neutral-text-secondary)]">
+          Đã bán {sold}/{totalQuantity}
+        </p>
+      </div>
+    </div>
+  );
+}
