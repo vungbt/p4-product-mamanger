d00d3e745829fcf3537610f48a56ad82d68f8abc
Bùi Phương Đông <dongbui942006@gmail.com>
2026-09-18 14:40:06 +0700

    Revert "Feat mobile sticky action bar"

--- libs/ui/components/icons/index.tsx
+++ libs/ui/components/icons/index.tsx
@@ -38,7 +38,6 @@ import {
   Handshake,
   Heading1,
   Heading2,
-  Heart,
   Home,
   Image,
   Inbox,
@@ -102,7 +101,6 @@ export type IconProps = {
 export type Icon = FC<IconProps>;
 
 const iconMap = {
-  heart: Heart,
   home: Home,
   'home-solid': Home,
   /** Alias matching the exact icon name in docs/design/p4-product-manager-design.html (Section A · Icon set) */
